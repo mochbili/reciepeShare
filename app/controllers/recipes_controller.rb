d3d05d0d585277e770b8bda88dcbcb00ca2f362d
mochbili <mochbili@gmail.com>
2016-10-18 14:42:10 +0700

    add actions edit and update on controller for recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -24,6 +24,20 @@ class RecipesController < ApplicationController
     end
   end
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = "Successfully updated..."
+      redirect_to recipe_path(@recipe)
+    else
+      render "edit"
+    end
+  end
+
   private
 
     def recipe_params
