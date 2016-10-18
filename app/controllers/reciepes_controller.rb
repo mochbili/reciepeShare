f3186c338de44d2db34cd93745b1329bb6b237f0
mochbili <mochbili@gmail.com>
2016-10-18 10:04:15 +0700

    add show action and view, and build the link for navigation

--- app/controllers/reciepes_controller.rb
+++ app/controllers/reciepes_controller.rb
@@ -3,4 +3,8 @@ class ReciepesController < ApplicationController
   def index
     @reciepes = Reciepe.order("created_at ASC")
   end
+
+  def show
+    @reciepe = Reciepe.find(params[:id])
+  end
 end
