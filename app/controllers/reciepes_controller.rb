15f3ab169c3eb628fdd415a6f263827e55e3e30c
mochbili <mochbili@gmail.com>
2016-10-18 11:19:05 +0700

    add new, create action and view, and add partial for flash messages then display the error validation for reciepe model

--- app/controllers/reciepes_controller.rb
+++ app/controllers/reciepes_controller.rb
@@ -7,4 +7,26 @@ class ReciepesController < ApplicationController
   def show
     @reciepe = Reciepe.find(params[:id])
   end
+
+  def new
+    @reciepe = Reciepe.new
+  end
+
+  def create
+    @reciepe = Reciepe.new(reciepe_params)
+    @reciepe.chef = Chef.find(1)
+
+    if @reciepe.save
+      flash[:success] = "Successfully created..."
+      redirect_to reciepes_path
+    else
+      render "new"
+    end
+  end
+
+  private
+
+    def reciepe_params
+      params.require(:reciepe).permit(:name, :summary, :description)
+    end
 end
